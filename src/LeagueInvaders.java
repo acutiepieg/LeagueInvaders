b659e2030d4f2ecc895fde3febee6c24e20966fc
Punky64 <league@WTS20.attlocal.net>
2017-12-13 16:57:56 -0800

    Pt. 5 #5

---
+++ src/LeagueInvaders.java
@@ -0,0 +1,36 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class LeagueInvaders {
+
+	final int wHeight = 800;
+	final int wWidth = 500;
+	
+	JFrame window;
+	GamePanel gp;
+	
+	public static void main(String[] args) {
+	LeagueInvaders li = new LeagueInvaders();
+li.setup();
+	}
+
+	public LeagueInvaders() {
+		window = new JFrame();
+			gp = new GamePanel();	
+		
+		}
+
+public void setup() {
+	window.add(gp);
+	window.setVisible(true);
+	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	window.setSize(wWidth, wHeight);
+	window.getContentPane().setPreferredSize(new Dimension(wWidth,wHeight));
+window.addKeyListener(gp);
+
+	gp.startGame();
+}
+
+
+}
